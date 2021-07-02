5cc63a7710b517e9ce27d4e5e27a67f80e690dce
manavchauhan18 <manavchauhan277@gmail.com>
2021-07-02 14:11:26 +0530

    added slick slider and scroll functionality

---
+++ static/js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $('.project-slider').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow: $('.slick-prev'),
+        nextArrow: $('.slick-next'),
+        dots: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: false,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+      $('.nav-trigger').click(function(){
+        $('.site-content-wrapper').toggleClass('scaled');
+     })
+})
