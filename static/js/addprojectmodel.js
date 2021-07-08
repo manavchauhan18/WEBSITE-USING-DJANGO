6110eef6ea69d7464136c0193e426bd91615e3d1
manavchauhan18 <manavchauhan277@gmail.com>
2021-07-08 14:14:37 +0530

    added slick add function and contact me section

--- static/js/addprojectmodel.js
+++ static/js/addprojectmodel.js
@@ -1,1 +1,10 @@
-var 
+var title = document.getElementById('projectname');
+var link = document.getElementById('projectlink');
+var image = document.getElementById('projectimage');
+
+var projectSlider = document.getElementsByClassName('slick-track');
+var projectCard = document.getElementById('projectcard');
+var projectImage = document.getElementById('projectimage');
+
+function addprojectmodel() {
+}
--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $('.project-slider').slick({
-        infinite: true,
+        infinite: false,
         speed: 120,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         prevArrow: $('.slick-prev'),
         nextArrow: $('.slick-next'),
         dots: false,
@@ -12,8 +12,8 @@ $(document).ready(function() {
             breakpoint: 1024,
             settings: {
               slidesToShow: 2,
-              slidesToScroll: 2,
-              infinite: true,
+              slidesToScroll: 1,
+              infinite: false,
               dots: false,
             }
           },
@@ -22,7 +22,7 @@ $(document).ready(function() {
             settings: {
               slidesToShow: 1,
               slidesToScroll: 1,
-              infinite: true,
+              infinite: false,
             }
           },
           {
@@ -30,7 +30,7 @@ $(document).ready(function() {
             settings: {
               slidesToShow: 1,
               slidesToScroll: 1,
-              infinite: true,
+              infinite: false,
             }
           }
           // You can unslick at a given breakpoint now by adding:
@@ -41,4 +41,11 @@ $(document).ready(function() {
       $('.nav-trigger').click(function(){
         $('.site-content-wrapper').toggleClass('scaled');
      })
+})
+
+var slideIndex = 6;
+$('.js-add-slide').on('click', function() {
+    slideIndex++;
+    $('.project-slider').slick('slickAdd',function addprojectmodel() {
+    });
 })
